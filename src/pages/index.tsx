5a1de41503e36dbb403a2d3221d1c5aa6f129a02
jotalp <holypants77@protonmail.com>
2023-04-07 07:39:57 -0400

    initial committ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import ContactForm from '@/pages/ContactForm'
 
 
 const inter = Inter({ subsets: ['latin'] })
